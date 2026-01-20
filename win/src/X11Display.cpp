f5acf4afe99409d70b5fe367a4d9952ecab661ac
Josh Winter <winterjoshr@gmail.com>
2026-01-19 21:10:41 -0500

    update win

--- win/src/X11Display.cpp
+++ win/src/X11Display.cpp
@@ -329,6 +329,12 @@ X11Display::~X11Display()
 
 void X11Display::process()
 {
+	if (resize_state.resized && std::chrono::duration<float>(std::chrono::steady_clock::now() - resize_state.time).count() > 0.25f)
+	{
+		resize_state.resized = false;
+		resize_handler(window_prop_cache.w, window_prop_cache.h);
+	}
+
 	while(XPending(xdisplay))
 	{
 		XEvent xevent;
@@ -353,7 +359,10 @@ void X11Display::process()
 					update_refresh_rate();
 
 				if (resized)
-					resize_handler(w, h);
+				{
+					resize_state.resized = true;
+					resize_state.time = std::chrono::steady_clock::now();
+				}
 
 				window_prop_cache.x = x;
 				window_prop_cache.y = y;
